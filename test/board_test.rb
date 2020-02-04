81faf094881b946cc6f9cd07e5c0a75981bd451b
Chris Stankus <chrisstankus99@gmail.com>
2020-02-04 12:21:59 -0700

    Fix indentation

--- test/board_test.rb
+++ test/board_test.rb
@@ -20,21 +20,21 @@ class BoardTest < Minitest::Test
   end
 
   def test_board_can_check_valid_placement
-  refute @board.valid_placement?(cruiser, ["A1", "A2"])
-  refute @board.valid_placement?(submarine, ["A2", "A3", "A4"])
-  assert @board.valid_placement?(submarine, ["A1", "A2"])
-  assert @board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+    refute @board.valid_placement?(cruiser, ["A1", "A2"])
+    refute @board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    assert @board.valid_placement?(submarine, ["A1", "A2"])
+    assert @board.valid_placement?(cruiser, ["B1", "C1", "D1"])
   end
 
   def test_board_can_check_if_valid_placement_is_consecutive
-  refute @board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-  refute @board.valid_placement?(submarine, ["A1", "C1"])
-  refute @board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-  refute @board.valid_placement?(submarine, ["C1", "B1"])
+    refute @board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    refute @board.valid_placement?(submarine, ["A1", "C1"])
+    refute @board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    refute @board.valid_placement?(submarine, ["C1", "B1"])
   end
 
   def test_board_can_check_valid_placement_is_not_diagonal
-  refute @board.valid_placement?(cruiser, ["A1", "B2", "C3"])
-  refute @board.valid_placement?(submarine, ["C2", "D3"])
+    refute @board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+    refute @board.valid_placement?(submarine, ["C2", "D3"])
   end
 end
