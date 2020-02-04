bef118f4cf38dd877e99ec9951df1bff76e2dae1
Chris Stankus <chrisstankus99@gmail.com>
2020-02-03 17:46:15 -0700

    Tests for cell attributes, empty method, place ship method, fired upon method, and fire upon method. All tests pass.

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,28 @@
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship, :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
+  end
+
+  def empty?
+    @ship == nil
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if @ship != nil
+      @ship.hit
+    end
+      @fired_upon = true
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -13,28 +13,25 @@ class CellTest < Minitest::Test
     assert_instance_of Cell, @cell
   end
 
+  def test_its_attributes
+    assert_equal "B4", @cell.coordinate
+    assert_nil @cell.ship
+  end
+
+  def test_cells_default_is_empty_and_can_contain_ship
+    assert @cell.empty?
+
+    @cell.place_ship(@cruiser)
+    assert_equal @cruiser, @cell.ship
+    refute @cell.empty?
+  end
+
+  def test_cell_can_be_fired_upon
+    @cell.place_ship(@cruiser)
+    refute @cell.fired_upon?
+
+    @cell.fire_upon
+    assert @cell.fired_upon?
+    assert_equal 2, @cruiser.health
+  end
 end
-
-
-# pry(main)> cell = Cell.new("B4")
-# # => #<Cell:0x00007f84f0ad4720...>
-#
-# pry(main)> cell.coordinate
-# # => "B4"
-#
-# pry(main)> cell.ship
-# # => nil
-#
-# pry(main)> cell.empty?
-# # => true
-#
-# pry(main)> cruiser = Ship.new("Cruiser", 3)
-# # => #<Ship:0x00007f84f0891238...>
-#
-# pry(main)> cell.place_ship(cruiser)
-#
-# pry(main)> cell.ship
-# # => #<Ship:0x00007f84f0891238...>
-#
-# pry(main)> cell.empty?
-# # => false
