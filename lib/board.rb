2e4123cd7af32b99f2fbf5dc6d71f3d3b907ce58
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-04 12:54:11 -0700

    Test for cells to be instance of board and cell Cells attr_reader

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,5 @@
 class Board
+  attr_reader :cells
 
   def initialize
     @cells = {
--- test/board_test.rb
+++ test/board_test.rb
@@ -11,7 +11,17 @@ class BoardTest < Minitest::Test
     @submarine = Ship.new("Submarine", 2)
   end
 
+  def test_it_exists
+    assert_instance_of Board, @board
+  end
+
+  def test_board_has_cells
+    assert_instance_of Hash, @board.cells
+    assert_instance_of Cell, @board.cells["A1"]
+  end
+
   def test_board_can_check_valid_coordinate
+    skip
     assert @board.valid_coordinate("A1")
     assert @board.valid_coordinate?("D4")
     refute @board.valid_coordinate?("A5")
@@ -20,6 +30,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_board_can_check_valid_placement
+    skip
     refute @board.valid_placement?(cruiser, ["A1", "A2"])
     refute @board.valid_placement?(submarine, ["A2", "A3", "A4"])
     assert @board.valid_placement?(submarine, ["A1", "A2"])
@@ -27,6 +38,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_board_can_check_if_valid_placement_is_consecutive
+    skip
     refute @board.valid_placement?(cruiser, ["A1", "A2", "A4"])
     refute @board.valid_placement?(submarine, ["A1", "C1"])
     refute @board.valid_placement?(cruiser, ["A3", "A2", "A1"])
@@ -34,6 +46,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_board_can_check_valid_placement_is_not_diagonal
+    skip
     refute @board.valid_placement?(cruiser, ["A1", "B2", "C3"])
     refute @board.valid_placement?(submarine, ["C2", "D3"])
   end
