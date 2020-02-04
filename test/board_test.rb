edaa30a7517b87839752c31e1efc9d914254d3f1
Chris Stankus <chrisstankus99@gmail.com>
2020-02-04 12:14:59 -0700

    Add test setup and check valid coordinate test

---
+++ test/board_test.rb
@@ -0,0 +1,21 @@
+require_relative 'test_helper'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+
+  def setup
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
+
+  def test_board_can_check_valid_coordinate
+    assert @board.valid_coordinate("A1")
+    assert @board.valid_coordinate?("D4")
+    refute @board.valid_coordinate?("A5")
+    refute @board.valid_coordinate?("E1")
+    refute @board.valid_coordinate?("A22")
+  end
+end
