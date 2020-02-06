66e7350acdb15873e578db395f34760db7bf1786
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-05 21:20:56 -0700

    Tests for validate placement Testing letter ordinates and consecutive thus far. All tests passing.

--- test/board_test.rb
+++ test/board_test.rb
@@ -28,7 +28,27 @@ class BoardTest < Minitest::Test
     refute @board.valid_coordinate?("A22")
   end
 
+#HELPER METHOD TESTS FOR VALID_PLACEMENT?
+
+  def test_ship_length_is_the_same_as_input_coordinates
+    assert_equal true, @board.appropriate_length?(@submarine, ["A1", "A2"])
+    assert_equal false, @board.appropriate_length?(@cruiser, ["B1", "B2", "C3", "D4"])
+  end
+
+  def test_letters_can_be_converted_to_ordinal_numbers
+    assert_equal [66,67,68], @board.letter_ordinates(["B1", "C1", "D1"])
+  end
+
+  def test_coordinates_are_consecutive
+    assert @board.consecutive?(["A1", "B1"])
+    refute @board.consecutive?(["A1", "A2", "A4"])
+  end
+
+
+#-----------------------------------------------------------------------------
+
   def test_board_can_check_valid_placement
+    skip
     refute @board.valid_placement?(@cruiser, ["A1", "A2"])
     refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
     assert @board.valid_placement?(@submarine, ["A1", "A2"])
@@ -48,4 +68,18 @@ class BoardTest < Minitest::Test
     refute @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
     refute @board.valid_placement?(@submarine, ["C2", "D3"])
   end
+
+  def test_ship_is_able_to_be_placed_on_board_and_can_occupy_multiple_cells
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    cell_1 = @board.cells["A1"]
+    cell_2 = @board.cells["A2"]
+    cell_3 = @board.cells["A3"]
+    assert_instance_of Ship, cell_1.ship
+    assert_instance_of Ship, cell_2.ship
+    assert_instance_of Ship, cell_3.ship
+
+
+
+  end
+
 end
