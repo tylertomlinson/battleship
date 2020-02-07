a561482382fc50cc83116b2e5deed4c8a2a22281
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-06 18:29:46 -0700

    Remove tests for helper methods

--- test/board_test.rb
+++ test/board_test.rb
@@ -28,48 +28,6 @@ class BoardTest < Minitest::Test
     refute @board.valid_coordinate?("A22")
   end
 
-#HELPER METHOD TESTS FOR VALID_PLACEMENT?
-  #
-  # def test_ship_length_is_the_same_as_input_coordinates
-  #   assert_equal true, @board.consecutive?(["A1", "A2"])
-  #   assert_equal true, @board.consecutive?(["C3", "C4"])
-  #   assert_equal true, @board.consecutive?(["A1", "B1"])
-  #   assert_equal false, @board.consecutive?(["B1", "B2", "C3", "D4"])
-  # end
-  # #
-  # def test_letters_can_be_converted_to_ordinal_numbers
-  #   skip
-  #   assert_equal [66,67,68], @board.letter_ordinates(["B1", "C1", "D1"])
-  # end
-  #
-  # def test_letter_coordinates_are_consecutive
-  #   skip
-  #   assert @board.lettersconsecutive?(["A1", "A2", "A3"])
-  #   assert_equal false, @board.consecutive?(["A1", "B1", "C1"])
-  # end
-  #
-  # def test_letter_coordinates_are_consecutive
-  #   skip
-  #   assert @board.letters_consecutive?(["A1", "B1", "C1"])
-  #   assert_equal false, @board.letters_consecutive?(["A1", "A2", "B1"])
-  # end
-  #
-  # def test_number_coordinates_are_consecutive
-  #   skip
-  #   assert @board.numbers_consecutive?(["A1", "A2", "A3"])
-  #   assert_equal false, @board.numbers_consecutive?(["A1", "A3", "A4"])
-  # end
-  #
-  # def test_horizontal_placement_has_is_correct
-  #   assert_equal false, @board.horizontal_placement?(["A1", "A2", "A4"])
-  #   assert_equal  @board.horizontal_placement?(["A1", "A2", "A3"])
-  # end
-  #
-  # def test_vertical_placement_has_unique_letter
-  #
-  # end
-#-----------------------------------------------------------------------------
-
   def test_board_can_check_valid_placement
     refute @board.valid_placement?(@cruiser, ["A1", "A2"])
     refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
