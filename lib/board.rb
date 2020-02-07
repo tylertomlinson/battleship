f5c069486930da1d5b362b082c86257d50a74f90
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-06 18:24:32 -0700

    Refactor needs work to pass

--- lib/board.rb
+++ lib/board.rb
@@ -26,12 +26,9 @@ class Board
     @cells.key?(coordinate)
   end
 
-
   def valid_placement?(ship, coordinates)
     appropriate_length?(ship, coordinates) && ships_cannot_overlap?(coordinates) && consecutive?(coordinates)
-
   end
-
 
   def appropriate_length?(ship, coordinates)
     coordinates.length == ship.length
@@ -46,23 +43,43 @@ class Board
   end
 
   def consecutive?(coordinates)
-    letters_cords = coordinates.map(&:chars).map {|coordinate| coordinate[0]}
-    numbers_cords = coordinates.map(&:chars).map {|coordinate| coordinate[1].to_i}
-    if letters_cords.uniq.size == 1 &&
-    numbers_cords.each_cons(2).all? do |number1, number2|
-      number2 == (number1 + 1)
-    end
-      true
-    elsif numbers_cords.uniq.size == 1 &&
-      letters_cords.map { |letter| letter.ord }.each_cons(2).all? {|number1, number2| number2 == (number1 + 1)}
-      true
-    else
-      false
-    end
+    consecutive_letter_coordinates?(coordinates) && consecutive_number_coordinates?(coordinates)
+  end
+
+  def letter_coordinates(coordinates)
+    coordinates.map(&:chars).map {|coordinate| coordinate[0]}
+  end
+
+  def number_coordinates(coordinates)
+    coordinates.map(&:chars).map {|coordinate| coordinate[1].to_i}
+  end
+
+  def consecutive_letter_coordinates?(coordinates)
+    letter_coordinates(coordinates).uniq.size == 1 &&
+    number_coordinates(coordinates).each_cons(2).all? { |number1, number2| number2 == (number1 + 1) }
+  end
+
+  def consecutive_number_coordinates?(coordinates)
+    number_coordinates(coordinates).uniq.size == 1 &&
+    letter_coordinates(coordinates).map { |letter| letter.ord }.each_cons(2).all? { |number1, number2| number2 == (number1 + 1) }
   end
 
   def place(ship, coordinates)
     coordinates.each { |coordinate| @cells[coordinate].place_ship(ship) }
   end
-
 end
+# def consecutive?(coordinates)
+#   letters_cords = coordinates.map(&:chars).map {|coordinate| coordinate[0]}
+#   numbers_cords = coordinates.map(&:chars).map {|coordinate| coordinate[1].to_i}
+#   if letters_cords.uniq.size == 1 &&
+#   numbers_cords.each_cons(2).all? do |number1, number2|
+#     number2 == (number1 + 1)
+#   end
+#     true
+#   elsif numbers_cords.uniq.size == 1 &&
+#     letters_cords.map { |letter| letter.ord }.each_cons(2).all? {|number1, number2| number2 == (number1 + 1)}
+#     true
+#   else
+#     false
+#   end
+# end
