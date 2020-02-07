31d1e160cacd062b4e53cf1636b40bb6da2d2d38
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-06 18:19:33 -0700

    PASSING VALID_PLACEMENT METHOD!

--- lib/board.rb
+++ lib/board.rb
@@ -3,48 +3,66 @@ class Board
 
   def initialize
     @cells = {
-        "A1" => Cell.new("A1"),
-        "A2" => Cell.new("A2"),
-        "A3" => Cell.new("A3"),
-        "A4" => Cell.new("A4"),
-        "B1" => Cell.new("B1"),
-        "B2" => Cell.new("B2"),
-        "B3" => Cell.new("B3"),
-        "B4" => Cell.new("B4"),
-        "C1" => Cell.new("C1"),
-        "C2" => Cell.new("C2"),
-        "C3" => Cell.new("C3"),
-        "C4" => Cell.new("C4"),
-        "D1" => Cell.new("D1"),
-        "D2" => Cell.new("D2"),
-        "D3" => Cell.new("D3"),
-        "D4" => Cell.new("D4"),
-        }
+      "A1" => Cell.new("A1"),
+      "A2" => Cell.new("A2"),
+      "A3" => Cell.new("A3"),
+      "A4" => Cell.new("A4"),
+      "B1" => Cell.new("B1"),
+      "B2" => Cell.new("B2"),
+      "B3" => Cell.new("B3"),
+      "B4" => Cell.new("B4"),
+      "C1" => Cell.new("C1"),
+      "C2" => Cell.new("C2"),
+      "C3" => Cell.new("C3"),
+      "C4" => Cell.new("C4"),
+      "D1" => Cell.new("D1"),
+      "D2" => Cell.new("D2"),
+      "D3" => Cell.new("D3"),
+      "D4" => Cell.new("D4"),
+    }
   end
 
   def valid_coordinate?(coordinate)
     @cells.key?(coordinate)
   end
 
+
   def valid_placement?(ship, coordinates)
-    appropriate_length?(ship, coordinates)
-
-
-    #helper methods for consecutive and diagonal
-    #break hash in numbers array and letters array
-    #uniq numbers via ordinal values
+    appropriate_length?(ship, coordinates) && ships_cannot_overlap?(coordinates) && consecutive?(coordinates)
 
   end
+
 
   def appropriate_length?(ship, coordinates)
     coordinates.length == ship.length
   end
 
-  # def consecutive_numbers(coordinates)
-  #   # coordinates.map {|coordinate|}
-  # end
-  #
-  # def consecutive_letters
-  # end
+  def ships_cannot_overlap?(coordinates)
+    coordinates.all? do |coordinate|
+      if @cells.key?(coordinate)
+        @cells[coordinate].empty?
+      end
+    end
+  end
+
+  def consecutive?(coordinates)
+    letters_cords = coordinates.map(&:chars).map {|coordinate| coordinate[0]}
+    numbers_cords = coordinates.map(&:chars).map {|coordinate| coordinate[1].to_i}
+    if letters_cords.uniq.size == 1 &&
+    numbers_cords.each_cons(2).all? do |number1, number2|
+      number2 == (number1 + 1)
+    end
+      true
+    elsif numbers_cords.uniq.size == 1 &&
+      letters_cords.map { |letter| letter.ord }.each_cons(2).all? {|number1, number2| number2 == (number1 + 1)}
+      true
+    else
+      false
+    end
+  end
+
+  def place(ship, coordinates)
+    coordinates.each { |coordinate| @cells[coordinate].place_ship(ship) }
+  end
 
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -28,6 +28,48 @@ class BoardTest < Minitest::Test
     refute @board.valid_coordinate?("A22")
   end
 
+#HELPER METHOD TESTS FOR VALID_PLACEMENT?
+  #
+  # def test_ship_length_is_the_same_as_input_coordinates
+  #   assert_equal true, @board.consecutive?(["A1", "A2"])
+  #   assert_equal true, @board.consecutive?(["C3", "C4"])
+  #   assert_equal true, @board.consecutive?(["A1", "B1"])
+  #   assert_equal false, @board.consecutive?(["B1", "B2", "C3", "D4"])
+  # end
+  # #
+  # def test_letters_can_be_converted_to_ordinal_numbers
+  #   skip
+  #   assert_equal [66,67,68], @board.letter_ordinates(["B1", "C1", "D1"])
+  # end
+  #
+  # def test_letter_coordinates_are_consecutive
+  #   skip
+  #   assert @board.lettersconsecutive?(["A1", "A2", "A3"])
+  #   assert_equal false, @board.consecutive?(["A1", "B1", "C1"])
+  # end
+  #
+  # def test_letter_coordinates_are_consecutive
+  #   skip
+  #   assert @board.letters_consecutive?(["A1", "B1", "C1"])
+  #   assert_equal false, @board.letters_consecutive?(["A1", "A2", "B1"])
+  # end
+  #
+  # def test_number_coordinates_are_consecutive
+  #   skip
+  #   assert @board.numbers_consecutive?(["A1", "A2", "A3"])
+  #   assert_equal false, @board.numbers_consecutive?(["A1", "A3", "A4"])
+  # end
+  #
+  # def test_horizontal_placement_has_is_correct
+  #   assert_equal false, @board.horizontal_placement?(["A1", "A2", "A4"])
+  #   assert_equal  @board.horizontal_placement?(["A1", "A2", "A3"])
+  # end
+  #
+  # def test_vertical_placement_has_unique_letter
+  #
+  # end
+#-----------------------------------------------------------------------------
+
   def test_board_can_check_valid_placement
     refute @board.valid_placement?(@cruiser, ["A1", "A2"])
     refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
@@ -36,7 +78,6 @@ class BoardTest < Minitest::Test
   end
 
   def test_board_can_check_if_valid_placement_is_consecutive
-    skip
     refute @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
     refute @board.valid_placement?(@submarine, ["A1", "C1"])
     refute @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
@@ -44,8 +85,20 @@ class BoardTest < Minitest::Test
   end
 
   def test_board_can_check_valid_placement_is_not_diagonal
-    skip
     refute @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
     refute @board.valid_placement?(@submarine, ["C2", "D3"])
   end
+
+  def test_ship_is_able_to_be_placed_on_board_and_can_occupy_multiple_cells
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+
+    cell_1 = @board.cells["A1"]
+    cell_2 = @board.cells["A2"]
+    cell_3 = @board.cells["A3"]
+
+    assert_instance_of Ship, cell_1.ship
+    assert_instance_of Ship, cell_2.ship
+    assert_instance_of Ship, cell_3.ship
+  end
+
 end
