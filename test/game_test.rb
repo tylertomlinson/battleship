39eac0d3d96bc5616e80961b1eb6f48578b20a32
Chris Stankus <chrisstankus99@gmail.com>
2020-02-08 19:45:29 -0700

    Add first test and setup for game test. (passes)

--- test/game_test.rb
+++ test/game_test.rb
@@ -2,11 +2,15 @@ require_relative 'test_helper'
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
+require './lib/game'
 
-# class Gametest < Minitest::Test
-#   def test_user_input_can_be_anything
-#     user_input = sanitize_input("a1a2a3")
-# 
-#     assert_equal ""
-#   end
-# end
+class Gametest < Minitest::Test
+
+  def setup
+    @game = Game.new
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @game
+  end
+end
