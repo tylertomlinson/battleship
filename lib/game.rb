b70cebadbbc1e77737017d671c2cb22f2e370953
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-10 11:27:44 -0700

    Moved things around Thinking about making a module for this will see. Also need to get this merged with master to move forward.

--- lib/game.rb
+++ lib/game.rb
@@ -2,12 +2,13 @@ require './lib/board'
 require './lib/cell'
 require './lib/ship'
 require './lib/player'
-require_relative 'output'
+require_relative 'output_module'
 
 class Game
   include OutputModule
 
-  attr_reader :user_board, :player_board, :cruiser, :submarine, :user, :computer, :cruiser_options, :sub_options
+  attr_accessor :user_board, :computer_board
+  attr_reader :user_board, :computer_board, :cruiser, :submarine, :user, :computer, :cruiser_options, :sub_options
 
   def initialize
     @user_board = Board.new
@@ -23,10 +24,9 @@ class Game
   def main_menu
     puts messages[:msg_1]
     # sleep 2
-      puts messages[:msg_2]
-      user_input = gets.chomp
-      menu_options(user_input)
-
+    puts messages[:msg_2]
+    user_input = gets.chomp
+    menu_options(user_input)
     start
   end
 
@@ -62,9 +62,8 @@ class Game
     @user_board.valid_placement?(@submarine, user_input)
 
     @user_board.place(@submarine, user_input)
-
     #board should render here with cruiser and submarine placed on whatever
-    #coordinates were passed in
+    #coordinates were passed in before turn starts?
     turn
   end
 
@@ -78,70 +77,74 @@ class Game
     user_guess = [] << gets.chomp.upcase
     @user.take_turn(user_guess)
     computer_guess = @computer.take_turn(user_guess)
-    require "pry"; binding.pry
+
+
     until valid_guess?(user_guess)
-    puts messages[:err_msg_3]
+      puts messages[:err_msg_3]
       user_guess = [] << gets.chomp.upcase
     end
     display_shot_results(computer_guess)
     turn
   end
 
-  def sanitized_input(user_input)
-    user_input.upcase.strip.gsub(" ", "").scan(/../)
-  end
+    #render boards could be its own method which is called within turn. need to test
 
-  def valid_input?(user_input)
-    if @user_board.valid_placement?(@cruiser, user_input) || @user_board.valid_placement?(@submarine, user_input)
-      true
-    else
-       messages[:err_msg_4]
-      input
+    def sanitized_input(user_input)
+      user_input.upcase.strip.gsub(" ", "").scan(/../)
     end
-  end
 
-  def valid_guess?(coordinate)
-    @computer_board.valid_coordinate?(coordinate[0])
+    def valid_input?(user_input)
+      if @user_board.valid_placement?(@cruiser, user_input) || @user_board.valid_placement?(@submarine, user_input)
+        true
+      else
+        messages[:err_msg_4]
+        input
+      end
+    end
 
-       # @computer_board.cell.fired_upon? == false
-  end
+    def valid_guess?(coordinate)
+      @computer_board.valid_coordinate?(coordinate[0])
+      # @computer_board.cell.fired_upon? == false
+
     #cell hasn't been fired upon
     # true
     # else
-      #prompt user that the guess is invalid and let them know what their guess was
+    #prompt user that the guess is invalid and let them know what their guess was
+    end
+
+    def display_shot_results(computer_guess)
+      require "pry"; binding.pry
+      if @user_board.cells[computer_guess].render == 'M'
+        "My shot on #{computer_guess} was a miss."
+      elsif @user_board.cells[computer_guess].render == 'H'
+        "My shot on #{computer_guess} was a hit."
+      elsif @user_board.cells[computer_guess].render == 'X'
+        "My shot on #{computer_guess} sunk your" #needs to display ship that was sunk.
+      end
+    end
 
 
-  def display_shot_results(computer_guess)
-    require "pry"; binding.pry
-    if @user_board.cells[computer_guess].render == 'M'
-      "My shot on #{computer_guess} was a miss."
-    elsif @user_board.cells[computer_guess].render == 'H'
-      "My shot on #{computer_guess} was a hit."
-    elsif @user_board.cells[computer_guess].render == 'X'
-      "My shot on #{computer_guess} sunk your" #needs to display ship that was sunk.
+    def user_ship_name(coordinate)
+      @user_board.cells[coordinate].ship.name
+    end
+
+    def menu_options(user_input)
+      if user_input == "p"
+        start
+      elsif user_input == "q"
+        puts messages[:ai_msg_6]
+        Process.exit!(true)
+      else
+        puts messages[:err_msg_1]
+      end
+    end
+
+    def end_game
+      #use message hash for outputs
+      return "You won!" if health(@computer_board) == 0
+      return "I won!" if health(@user_board) == 0
+      return "You ended the game" if end_game
     end
   end
 
-  def user_ship_name(coordinate)
-    @player_board.cells[coordinate].ship.name
-  end
-
-  def menu_options(user_input)
-    if user_input == "p"
-      start
-    elsif user_input == "q"
-      puts messages[:ai_msg_6]
-      Process.exit!(true)
-    else
-    puts messages[:err_msg_1]
-    end
-  end
-
-  def end_game
-    return "You won!" if health(@computer_board) == 0
-    return "I won!" if health(@user_board) == 0
-    return "You ended the game" if end_game
-  end
-end
-
-#THIS IS BROKEN. HAVE NO IDEA WHAT IS GOING ON.
+  #THIS IS BROKEN. HAVE NO IDEA WHAT IS GOING ON.
