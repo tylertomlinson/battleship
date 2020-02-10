6d39a73dce16ca67095221e22974d345c37437c5
Chris Stankus <chrisstankus99@gmail.com>
2020-02-10 07:34:42 -0700

    Add valid cruiser and submarine input methods. Split up input method.

--- lib/game.rb
+++ lib/game.rb
@@ -7,11 +7,11 @@ require_relative 'output'
 class Game
   include OutputModule
 
-  attr_reader :board, :cruiser, :submarine, :user, :computer
+  attr_reader :board, :cruiser, :submarine, :user, :computer, :user_board, :computer_board
 
   def initialize
     @user_board = Board.new
-    @player_board = Board.new
+    @computer_board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
     @user = Player.new
@@ -23,8 +23,8 @@ class Game
   def main_menu
     puts messages[:msg_1]
     # sleep 2
+    loop do
       puts messages[:msg_2]
-      loop do
       user_input = gets.chomp
       menu_options(user_input)
     end
@@ -41,29 +41,34 @@ class Game
     # sleep 1.5
     puts messages[:msg_5]
     # sleep 1.5
-    puts messages[:msg_6]
+    # puts messages[:msg_6]
     # sleep 0.5
-    input
+    cruiser_input
   end
 
-  def input
+  def cruiser_input
+    puts messages[:msg_6]
     user_input = gets.chomp
     user_input = sanitized_input(user_input)
-    valid_input?(user_input)
-    @board.valid_placement?(@cruiser, user_input)
+    valid_cruiser_input?(user_input)
+    @user_board.valid_placement?(@cruiser, user_input)
 
-    @board.place(@cruiser, user_input)
-    puts @board.render(true)
+    @user_board.place(@cruiser, user_input)
+    puts @user_board.render(true)
     #board should render here with cruiser placed on whatever coordinates were
     #passed in
+    submarine_input
+  end
+
+  def submarine_input
     puts messages[:msg_7]
     user_input = gets.chomp
     user_input = sanitized_input(user_input)
-    valid_input?(user_input)
-    @board.valid_placement?(@submarine, user_input)
+    valid_submarine_input?(user_input)
+    @user_board.valid_placement?(@submarine, user_input)
 
-    @board.place(@submarine, user_input)
-
+    @user_board.place(@submarine, user_input)
+    puts @user_board.render(true)
     #board should render here with cruiser and submarine placed on whatever
     #coordinates were passed in
     turn
@@ -72,9 +77,9 @@ class Game
 
   def turn
     puts messages[:msg_8]
-    @player.opponent_board.render
+    @computer_board.render
     puts messages[:msg_9]
-    @player.your_board.render(true)
+    @user_board.render(true)
 
     puts  messages[:msg_10]
 
@@ -96,22 +101,23 @@ class Game
 
   def sanitized_input(user_input)
     user_input = user_input.upcase.strip.gsub(" ", "").scan(/../)
-    fixed_input = []
-    user_input.each do |input|
-      if @board.cells.keys.include?(input)
-        fixed_input << input
-      end
-    end
-    fixed_input
   end
 
-  def valid_input?(user_input)
 
-    if @board.valid_placement?(@cruiser, user_input) || @board.valid_placement?(@submarine, user_input)
-      true
+
+  def valid_cruiser_input?(user_input)
+    if @user_board.valid_placement?(@cruiser, user_input)
     else
-      messages[:err_msg_4]
-      input
+      puts messages[:err_msg_4]
+      cruiser_input
+    end
+  end
+
+  def valid_submarine_input?(user_input)
+    if @user_board.valid_placement?(@submarine, user_input)
+    else
+      puts messages[:err_msg_4]
+      submarine_input
     end
   end
 
--- test/game_test.rb
+++ test/game_test.rb
@@ -17,7 +17,7 @@ class Gametest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_instance_of Board, @game.board
+    assert_instance_of Board, @game.user_board
     assert_instance_of Ship, @game.cruiser
     assert_instance_of Ship, @game.submarine
   end
@@ -25,11 +25,15 @@ class Gametest < Minitest::Test
   def test_sanitized_input
     assert_equal ["A1","A2","A3"], @game.sanitized_input("a1a2a3")
     assert_equal ["B1", "B2", "B3"], @game.sanitized_input("b  1  b2   b  3")
-    assert_equal ["C1", "C2"], @game.sanitized_input("aaC   1  c2  aa")
+  end
+
+  def test_valid_cruiser_input
+    # assert_equal false, @game.valid_cruiser_input?(["A1", "A2", "A1" ])
+    # require "pry"; binding.pry
   end
 
   def test_menu_options
-    assert_equal messages[:err_msg_1], @game.menu_options("w")
+    # assert_equal messages[:err_msg_1], @game.menu_options("w")
     # assert_equal @game.start, @game.menu_options("p")
   end
 
