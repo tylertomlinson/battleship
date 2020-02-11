0f64cd04ce09a9d96240e4ae4c6a9e506ec61629
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-11 15:48:16 -0700

    Change testing to mimic what output

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -6,10 +6,11 @@ require './lib/board'
 require './lib/player'
 require './lib/computer'
 
-class Test < Minitest::Test
+class ComputerTest < Minitest::Test
 
   def setup
     @computer = Computer.new
+    @board = Board.new
   end
 
   def test_it_exists
@@ -17,16 +18,23 @@ class Test < Minitest::Test
   end
 
   def test_computer_can_place_cruiser
-    @computer.stubs(:computer_place_cruiser).returns(["A1", "A2", "A3"])
-    @computer.stubs(:computer_place_cruiser).returns(["C2", "C3", "C4"])
+    possible_results = [["A1", "A2", "A3"], ["C2", "C3", "C4"]]
+
+    assert_equal true, possible_results.include?(@computer.computer_cruiser_placement)
   end
 
   def test_computer_can_place_submarine
-    @computer.stubs(:computer_place_submarine).returns(["D1", "D2"])
-    @computer.stubs(:computer_place_submarine).returns(["A4", "B4"])
+    possible_results = [["D1", "D2"], ["A4", "B4"]]
+
+    assert_equal true, possible_results.include?(@computer.computer_submarine_placement)
   end
 
-  def test_computer_can_take_turn
-    @computer.stubs(:gets).returns("C3")
-  end
+  # def test_computer_can_take_turn
+  #     Array.stubs(:shuffle).returns("C3")
+  #   assert_equal "C3", @computer.computer_take_turn
+  # end
+
+  # def test_computer_can_take_turn
+  #   assert_instance_of Cell,
+  # end
 end
