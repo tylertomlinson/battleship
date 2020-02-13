1a21994e24ef6f86e57f162ae66c84577f84bebb
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-13 02:50:24 -0700

    Change to one instance variable of player

--- test/player_test.rb
+++ test/player_test.rb
@@ -2,59 +2,44 @@ require_relative 'test_helper'
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
+require './lib/computer'
 require './lib/player'
 
-class Test < Minitest::Test
+class PlayerTest < Minitest::Test
 
   def setup
-    @player1 = Player.new
-    @player2 = Player.new
+    @player = Player.new
     @ship = Ship.new("Cruiser", 3)
     @coordinates = ["A1", "A2", "A3"]
-    @cruiser_options = [["A1", "A2", "A3"], ["C2", "C3", "C4"]].sample
-    @sub_options = [["D1", "D2"], ["A4", "B4"]].sample
+    @computer_board = Board.new
   end
 
   def test_it_exists
-    assert_instance_of Player, @player1
-    assert_instance_of Player, @player2
-
+    assert_instance_of Player, @player
   end
 
   def test_player_has_own_board_and_opponent_board
-    assert_instance_of Board , @player1.board
-    assert_instance_of Board, @player1.board
-
-    assert_instance_of Board, @player2.board
-    assert_instance_of Board, @player2.board
+    assert_instance_of Board, @player.board
   end
 
   def test_user_has_entered_valid_coordinates
-    assert @player1.valid_ship_placement(@ship, @coordinates)
+    assert @player.valid_ship_placement(@ship, @coordinates)
 
     test_coordinates = ["A2", "C4", "B1"]
 
-    refute @player1.valid_ship_placement(@ship, test_coordinates)
+    refute @player.valid_ship_placement(@ship, test_coordinates)
   end
-
-  def test_opponent_has_entered_valid_coordinates
-    assert @player2.valid_ship_placement(@ship, @cruiser_options)
-
-    test_coordinates = ["A2", "C4", "B1"]
-
-    refute @player2.valid_ship_placement(@ship, test_coordinates)
-  end
-
+  
   def test_player_can_place_ship_and_board_renders
-    @player1.valid_ship_placement(@ship, @coordinates)
-    @player1.ship_placement(@ship, @coordinates)
+    @player.valid_ship_placement(@ship, @coordinates)
+    @player.ship_placement(@ship, @coordinates)
 
-    assert @player1.board.render(true).include?("S")
+    assert @player.board.render(true).include?("S")
   end
 
   def test_player_can_take_turn
-    @player1.take_turn(["A1"])
+    @player.take_turn(["A1"], @computer_board)
 
-    assert @player1.board.cells["A1"].fired_upon?
+    assert @computer_board.cells["A1"].fired_upon?
   end
 end
