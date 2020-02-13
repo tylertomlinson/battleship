5d163f9fdec6eda2c95234888c06eb4f71a27a15
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-12 23:30:11 -0700

    Refactor

--- lib/game.rb
+++ lib/game.rb
@@ -4,9 +4,11 @@ require './lib/ship'
 require './lib/player'
 require './lib/computer'
 require_relative 'output_module'
+require_relative 'game_validation_module'
 
 class Game
   include OutputModule
+  include GameValidationModule
 
   attr_reader :cruiser, :submarine, :user, :computer
 
@@ -20,9 +22,9 @@ class Game
   end
 
   def main_menu
-    puts messages[:msg_1]
+    puts messages[:welcome]
     # sleep 2
-    puts messages[:msg_2]
+    puts messages[:play_quit]
     loop do
       user_input = gets.chomp
       menu_options(user_input)
@@ -33,15 +35,14 @@ class Game
   def start
     @computer.computer_cruiser_placement(@computer_board)
     @computer.computer_submarine_placement(@computer_board)
-    puts messages[:msg_3]
-    # sleep 1.5
-    puts messages[:msg_4]
-    # sleep 1.5
-    puts messages[:msg_5]
-    # sleep 1.5
+    puts messages[:comp_layout1]
+    resting
+    puts messages[:comp_layout2]
+    resting
+    puts messages[:user_layout]
+    resting
     cruiser_coordinates
   end
-  # until @player.all_ships_sunk? || @computer.all_ships_sunk? do
 
   def turn
     until game_over? do
@@ -76,11 +77,10 @@ class Game
     false
   end
 
-
   def boards
-    puts messages[:msg_8]
+    puts messages[:comp_board]
     puts @computer_board.render
-    puts messages[:msg_9]
+    puts messages[:user_board]
     puts @player_board.render(true)
   end
 
@@ -88,86 +88,12 @@ class Game
     if user_input == "p"
       start
     elsif user_input == "q"
-      puts messages[:ai_msg_6]
+      blinking
+      sample_quit_msgs
       Process.exit!(true)
     else
-      puts messages[:err_msg_1]
-    end
-  end
-
-  def player_feedback(user_guess)
-    if @computer_board.cells[user_guess[0]].render == "M"
-      puts "Your shot on #{user_guess[0]} was a miss!"
-    elsif @computer_board.cells[user_guess[0]].render == "H"
-      puts "Your shot on #{user_guess[0]} was a hit!"
-    elsif @computer_board.cells[user_guess[0]].render == "X"
-      puts "Your shot on #{user_guess[0]} sunk my battleship!!"
-    end
-  end
-
-  def computer_feedback
-    if @player_board.cells[@computer.shot_guesses.last].render == "M"
-      puts "My shot on #{@computer.shot_guesses.last} was a miss!"
-    elsif @player_board.cells[@computer.shot_guesses.last].render == "H"
-      puts "My shot on #{@computer.shot_guesses.last} was a hit!"
-    elsif @player_board.cells[@computer.shot_guesses.last].render == "X"
-      puts "My shot on #{@computer.shot_guesses.last} sunk your battleship!!"
-    end
-  end
-
-  def sanitized_input(user_input)
-    user_input = user_input.upcase.squeeze.strip.gsub(" ", "").scan(/../)
-  end
-
-  def valid_cruiser_input?(user_input)
-    while !@player_board.valid_placement?(@cruiser, user_input)
-      puts messages[:err_msg_4]
-      cruiser_input
-    end
-  end
-
-  def cruiser_coordinates
-    puts messages[:msg_6]
-    # sleep 0.5
-    cruiser_input
-    puts messages[:msg_6]
-    # sleep 0.5
-    cruiser_input
-  end
-
-  def cruiser_input
-    user_input = gets.chomp
-    user_input = sanitized_input(user_input)
-    valid_cruiser_input?(user_input)
-    @player_board.valid_placement?(@cruiser, user_input)
-
-    @player_board.place(@cruiser, user_input)
-    puts @player_board.render(true)
-    #board should render here with cruiser placed on whatever coordinates were
-    #passed in
-    submarine_coordinates
-  end
-
-  def submarine_coordinates
-    puts messages[:msg_7]
-    # sleep 0.5
-    submarine_input
-  end
-
-  def submarine_input
-    user_input = gets.chomp
-    user_input = sanitized_input(user_input)
-    valid_submarine_input?(user_input)
-    @player_board.place(@submarine, user_input)
-    puts @player_board.render(true)
-    @player_board.place(@submarine, user_input)
-    turn
-  end
-
-  def valid_submarine_input?(user_input)
-    while !@player_board.valid_placement?(@submarine, user_input)
-      puts messages[:err_msg_4]
-      submarine_input
+      blinking
+      puts error_messages[:invalid_start]
     end
   end
 end
